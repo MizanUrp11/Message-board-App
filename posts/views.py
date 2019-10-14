f0830a4fe181d6dd746f0235f721ad2f84c70ee4
MizanUrp11 <mrn1105009@gmail.com>
2019-10-15 00:07:18 +0600

    Message Posting

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
+from .models import Post
 from django.shortcuts import render
 
+from django.views.generic import ListView
 # Create your views here.
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
